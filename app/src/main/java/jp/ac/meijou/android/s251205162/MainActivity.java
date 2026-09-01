5d188ae0841f2cdcd6e2ee9d54f43a3f06f89407
Hoppy <251205162@ccmailg.meijo-u.ac.jp>
2026-09-01 15:45:40 +0900

    ボタンとテキストボックスを追加しました。

--- app/src/main/java/jp/ac/meijou/android/s251205162/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s251205162/MainActivity.java
@@ -1,6 +1,8 @@
 package jp.ac.meijou.android.s251205162;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +16,7 @@ import jp.ac.meijou.android.s251205162.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +27,83 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Changeボタンが押されたらtextViewにstringのtext2を設定する
+        /*binding.button.setOnClickListener(view ->{
+            binding.textView.setText(R.string.text2);
+        });*/
+
+        // Changeボタンが押されたらtextboxに入力された文字列をtextviewに設定する
+        binding.changeButton.setOnClickListener(view ->{
+            String text = binding.editTextText.getText().toString();
+            binding.textView.setText(text);
+        });
+
+        // ボタンが押されずともtextboxが変更されたらtextviewをリアルタイムで変更する
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String text = editable.toString();
+                binding.textView.setText(text);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+
+            }
+        });
+
+        // データストア
+        prefDataStore = PrefDataStore.getInstance(this);
+        // prefDataStore.getString("name").ifPresent(name -> binding.textView.setText(name));
+
+        // 入力によって表示する画像を変える
+        prefDataStore.getString("name").ifPresent(name ->
+        {if ("a".equals(name)) {
+            binding.textView.setText("Aの画像");
+            binding.imageView2.setImageResource(R.drawable.antigravity);
+        }else if ("b".equals(name)) {
+            binding.textView.setText("Bの画像");
+            binding.imageView2.setImageResource(R.drawable.baseline_cable_24);
+        }else {
+            binding.textView.setText("知らない画像");
+            binding.imageView2.setImageResource(R.drawable.ic_launcher_foreground);
+        }});
+
+        // saveボタンが押されたらtextboxを保存する
+        binding.saveButton.setOnClickListener(view -> {
+            // 入力がaかbを含んでいるかどうかによって画像が変わる場合の処理
+            String text = binding.editTextText.getText().toString();
+            if ("a".equals(text)) {
+                binding.imageView2.setImageResource(R.drawable.antigravity);
+                binding.textView.setText("Aの画像");
+            }else if ("b".equals(text)) {
+                binding.imageView2.setImageResource(R.drawable.baseline_cable_24);
+                binding.textView.setText("Bの画像");
+            }else {
+                binding.imageView2.setImageResource((R.drawable.ic_launcher_foreground));
+                binding.textView.setText("unknown");
+            }
+            prefDataStore.setString("name",text);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        /*
+        // textViewにstringのtext1を設定する(方法1)
         TextView textView = findViewById(R.id.text_view);
-        textView.setText(R.string.text2);
+        textView.setText(R.string.text1);
 
-        binding.textView.setText(R.string.text2);
+        // textViewにstringのtext1を設定する(方法2)
+        binding.textView.setText(R.string.text1);
+         */
     }
 }
